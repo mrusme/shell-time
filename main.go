e36f110c41ba5736b9c527764e8de60761b1aba0
マリウス <marius@xn--gckvb8fzb.com>
2023-09-05 21:09:59 -0500

    Implemented initial main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mrusme/shell-time/history"
+)
+
+func main() {
+	hist, err := history.New("zsh", "/home/mrus/.zsh_history")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("%v", hist.GetLines()[1337])
+}
