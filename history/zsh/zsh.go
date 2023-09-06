5d73d3e0bc7a1d32fd178f4bc52d3d8a30290bc0
マリウス <marius@xn--gckvb8fzb.com>
2023-09-05 21:32:40 -0500

    Implemented changed interface, lines type

--- history/zsh/zsh.go
+++ history/zsh/zsh.go
@@ -4,10 +4,20 @@ import (
 	"bufio"
 	"io"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
+type HistoryLine struct {
+	line      string
+	timestamp time.Time
+	command   string
+	args      string
+}
+
 type History struct {
-	lines []string
+	lines []HistoryLine
 }
 
 func (history *History) Initialize(historyFile string) error {
@@ -29,7 +39,23 @@ func (history *History) Initialize(historyFile string) error {
 			if c == ':' &&
 				len(tmpline) > 0 &&
 				tmpline[len(tmpline)-1:] == string('\n') {
-				history.lines = append(history.lines, tmpline)
+				t, terr := strconv.ParseInt(tmpline[2:12], 10, 64)
+				if terr != nil {
+					return terr
+				}
+				tmpsplit := strings.SplitN(tmpline, ";", 2)
+				cmdsplit := strings.SplitN(tmpsplit[1], " ", 2)
+				cmdargs := ""
+				if len(cmdsplit) > 1 {
+					cmdargs = cmdsplit[1]
+				}
+
+				history.lines = append(history.lines, HistoryLine{
+					line:      tmpline,
+					timestamp: time.Unix(t, 0),
+					command:   cmdsplit[0],
+					args:      cmdargs,
+				})
 				tmpline = ""
 			}
 			tmpline += string(c)
@@ -38,6 +64,10 @@ func (history *History) Initialize(historyFile string) error {
 	return nil
 }
 
-func (history *History) GetLines() []string {
-	return history.lines
+func (history *History) GetLines() []*string {
+	var lines []*string
+	for i := 0; i < len(history.lines); i++ {
+		lines = append(lines, &history.lines[i].line)
+	}
+	return lines
 }
