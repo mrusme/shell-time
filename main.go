a45a102d2a92f6d04a7544ce05d8da844cf82d0e
マリウス <marius@xn--gckvb8fzb.com>
2023-09-05 22:50:28 -0500

    Implemented draft stats

--- main.go
+++ main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"sort"
 
 	"github.com/mrusme/shell-time/history"
 )
+
+type CommandStats struct {
+	Command string
+	Count   int64
+}
+
+type Stats struct {
+	Commands map[string]CommandStats
+}
 
 func main() {
 	// TODO: Filepath
@@ -15,5 +26,39 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("%v", (*hist.GetLines()[1]))
+	var stats Stats
+	stats.Commands = make(map[string]CommandStats)
+
+	numLines := hist.GetNumberOfLines()
+	for i := int64(0); i < numLines; i++ {
+		_, cmd, _, err := hist.GetLine(i)
+		if err != nil {
+			// TODO: Handle error
+			continue
+		}
+
+		cmd = filepath.Base(cmd)
+
+		cmdstat, ok := stats.Commands[cmd]
+		if !ok {
+			cmdstat = CommandStats{}
+		}
+
+		cmdstat.Command = cmd
+		cmdstat.Count++
+
+		stats.Commands[cmd] = cmdstat
+	}
+
+	keys := make([]string, 0, len(stats.Commands))
+	for key := range stats.Commands {
+		keys = append(keys, key)
+	}
+	sort.SliceStable(keys, func(i, j int) bool {
+		return stats.Commands[keys[i]].Count < stats.Commands[keys[j]].Count
+	})
+
+	for _, cmd := range keys {
+		fmt.Printf("%s: %d\n", stats.Commands[cmd].Command, stats.Commands[cmd].Count)
+	}
 }
