03bcc597505c24355659efe880b17573ba5b7337
マリウス <marius@xn--gckvb8fzb.com>
2023-10-03 17:31:58 -0500

    Updated output

--- main.go
+++ main.go
@@ -26,9 +26,26 @@ func main() {
 	luCommands := s.LeastUsedCommands(10)
 	topHours := s.TopHours(5)
 
-	fmt.Printf("%v\n\n", topCommands)
-	fmt.Printf("%v\n\n", luCommands)
-	fmt.Printf("%v\n\n", topHours)
-	fmt.Printf("%v\n\n", s.MinutesPerDay)
-	fmt.Printf("%d\n\n", s.AverageMinutesPerDay)
+	fmt.Println()
+	fmt.Println("=== YOUR TOP 10 COMMANDS ===")
+	for i := 0; i < len(topCommands); i++ {
+		fmt.Printf("%2d. %s (%d times)\n", (i + 1), topCommands[i].Command, topCommands[i].Count)
+	}
+
+	fmt.Println()
+	fmt.Println("=== LONG FORGOTTEN COMMANDS ===")
+	for i := 0; i < len(luCommands); i++ {
+		fmt.Printf("%2d. %s\n", (i + 1), luCommands[i].Command)
+	}
+
+	fmt.Println()
+	fmt.Println("=== MOST PRODUCTIVE HOURS ===")
+	for i := 0; i < len(topHours); i++ {
+		fmt.Printf("%2d. %d:00 (%d commands fired)\n", (i + 1), topHours[i].Hour, topHours[i].Count)
+	}
+
+	fmt.Println()
+
+	// fmt.Printf("%v\n\n", s.MinutesPerDay)
+	fmt.Printf("On average you ran commands on the shell for about %d minutes per day.\n\n", s.AverageMinutesPerDay)
 }
